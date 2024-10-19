a0aaac40372e2abbad1ed50efbfa1cfb0879eec4
Damoz1606 <panchitodmun@gmail.com>
2024-10-19 14:55:15 -0500

    Budget request dto validation

--- src/budget/dto/request/buget.base.dto.ts
+++ src/budget/dto/request/buget.base.dto.ts
@@ -1,11 +1,13 @@
 import { Budget } from "@prisma/client";
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, MaxLength, Min } from "class-validator";
 
 export class BudgetRequest implements Omit<Budget, 'id' | 'budget' | 'userId'> {
     @IsString()
     @IsNotEmpty()
+    @MaxLength(64)
     public readonly name: string;
 
     @IsNumber()
+    @Min(0.01)
     public readonly budget: number;
 }
