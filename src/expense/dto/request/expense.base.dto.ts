29346b81983bb645aef19a5232e2b44799aaf28a
Damoz1606 <panchitodmun@gmail.com>
2024-10-19 14:55:44 -0500

    Expense request dto validation

--- src/expense/dto/request/expense.base.dto.ts
+++ src/expense/dto/request/expense.base.dto.ts
@@ -1,12 +1,14 @@
 import { Expense } from "@prisma/client";
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, MaxLength, Min } from "class-validator";
 
 export class ExpenseRequest implements Omit<Expense, 'id' | 'createAt' | 'budgetId' | 'amount'> {
     @IsString()
     @IsNotEmpty()
+    @MaxLength(64)
     public readonly name: string;
 
     @IsNumber()
+    @Min(0.01)
     public readonly amount: number;
 
     @IsNumber()
